1cf7da522212decf05ece048a7625baf2904dc75
Evan Hall <evan.tom.hall@gmail.com>
2021-05-02 12:15:22 -0400

    Add more __init__ overloads
    Pylance expects at least 2 overloads.

--- typings/cv2/videowriter.pyi
+++ typings/cv2/videowriter.pyi
@@ -3,13 +3,29 @@ import typing
 import numpy
 
 from .core.types import Size
+from .videoio import VideoCaptureAPIs
 
 class VideoWriter:
-    def __init__(self) -> None: ...
     @typing.overload
     def __init__(
-        self, filename: str, fourcc: int, fps: float, frameSize: Size
+        self,
+        filename: str,
+        apiPreference: VideoCaptureAPIs,
+        fourcc: int,
+        fps: float,
+        frameSize: Size,
+        isColor: bool = ...,
     ) -> None: ...
+    @typing.overload
+    def __init__(
+        self,
+        filename: str,
+        fourcc: int,
+        fps: float,
+        frameSize: Size,
+        isColor: bool = ...,
+    ) -> None: ...
+    def __init__(self) -> None: ...
     def write(self, image: numpy.ndarray) -> None: ...
     def release(self) -> None: ...
 
